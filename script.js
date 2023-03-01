427816217519a6d8e55b6eca4fb8b3647313f3cd
Brian Becerra <64347425+brian0107@users.noreply.github.com>
2023-02-28 23:13:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let botones = document.querySelectorAll("button");
 
-botones.forEach(function(elem) {
+botones.forEach(function(elem,e) {
     elem.addEventListener("click", function() {
         if(elem.textContent ==  "Comprar"){
             elem.textContent = "Agregado al carrito";
             elem.style.backgroundColor = "green";
-            elem.style.color = "white"
-            elem.parentElement.style.backgroundColor = 'blue';
+            elem.style.color = "white";
+            elem.parentElement.style.backgroundColor = 'yellow';
         }else{
             elem.textContent =  "Comprar";
             elem.style.backgroundColor = "white";
-            elem.style.color = "black"; 
+            elem.style.color = "black";
             elem.parentElement.style.backgroundColor = 'white';
-        }
-        
+        } 
     });
 });
